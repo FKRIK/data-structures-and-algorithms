ab21c4a76c4f5e5f08e08f7e5518458d27409fb3
FKRIK <fernandokrik@hotmail.com>
2023-05-23 11:56:54 -0300

    doing: php_RomanNumerals

--- php/algorithms/RomanNumerals.php
+++ php/algorithms/RomanNumerals.php
@@ -1,30 +1,68 @@
 <?php
 
-define("VALUES", [
-    1 => 'I',
-    5 => 'V',
-    10 => 'X',
-    50 => 'L',
-    100 => 'C',
-    500 => 'D',
-    1000 => 'M'
-]);
+function transcribeToRomanNumerals(int $inputNumber)
+{
+    $numbers = [
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000
+    ];
 
-function transcribeToRomanNumerals(int $inputNumber){
     echo "The input number is {$inputNumber}" . PHP_EOL;
-    
-    // print_r(array_keys(VALUES));die;
 
-    if (array_key_exists($inputNumber, VALUES)){
-        echo "It is the same number: "  . PHP_EOL;
-    }
-    
-    // foreach (VALUES as $number) {
-    //     break;
+    // for ($contador = 0; $contador < sizeof(NUMBERS); $contador++) {
+    //     if(array_key_first())
+    // }
+
+    $value = 10;
+    // var_dump(array_search($value, $numbers, true));
+    // die;
+
+    var_dump(array_search($value, $numbers));die;
+
+
+    // foreach(NUMBERS as $value){
+    //     if (key() != $inputNumber) continue;
+    // }
+
+
+    echo key(NUMBERS);
+
+    // if (key_exists($inputNumber, NUMBERS)){
+    //     echo "{$inputNumber} in Roman Numerals is " . key(NUMBERS) . PHP_EOL;
+    //     // echo "The key exists!";
+    // }
+
+    // foreach (NUMBERS as $value){
+    //     if (!key_exists($inputNumber, NUMBERS)){
+    //         continue;
+    //     }
+
+    //     echo "{$inputNumber} in Roman Numerals is {$value}" . PHP_EOL;
+    // }
+
+
+    // foreach (NUMBERS as $value) {
+    //     if (key($value) === $inputNumber){
+    //         echo "{$inputNumber} in Roman Numerals is " . key($value);
+    //     }
+    // }
+
+    echo "Teste";
+
+    // if (array_key_exists($inputNumber, NUMBERS)){
+    //     echo "{$inputNumber} in Roman Numerals is  ";
+    // }
+
+    // foreach (NUMBERS as $number) {
+    //     echo $number . PHP_EOL;
     // }
 
     return;
-    
 }
 
 transcribeToRomanNumerals(50);
