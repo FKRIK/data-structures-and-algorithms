7f2366a05a5f3c4bcf38b7f426f41b4169752697
FKRIK <fernandokrik@hotmail.com>
2023-08-31 20:07:07 -0300

    fixed: removed unused content

--- java/BubbleSort/Main.java
+++ java/BubbleSort/Main.java
@@ -1,90 +1,79 @@
-import java.util.Arrays;
-import java.util.Random;
-
-public class Main {
+public class Main
+{
 
 	public static void main(String[] args)
 	{
-		int size = 20;
-		int[] arrayNumeros = new int[size];
+            int size = 20;
+            int[] arrayNumeros = new int[size];
 		
-		System.out.println("Random array: ");
-		generateRandomArray(arrayNumeros);
+            System.out.println("Random array: ");
+            generateRandomArray(arrayNumeros);
 		
-		System.out.println("Bubble Sort: ");
-		bubbleSortDoWhile(arrayNumeros);
-		
-//		Arrays.sort(arrayNumeros);
-//		System.out.println("Esperado: ");
-//		System.out.print("[");
-//		for (int i : arrayNumeros)
-//		{
-//			System.out.print(" " + i + " ");
-//		}
-//		System.out.println("]");
+            System.out.println("Bubble Sort: ");
+            bubbleSortDoWhile(arrayNumeros);
 	}
 
 	public static void generateRandomArray(int[] array)
 	{
-		System.out.print("[");
-		for (int i = 0; i < array.length; i++)
-		{
-			array[i] = (int)(Math.random() * 501);
-			System.out.print(" " + array[i] + " ");
-		}
-		System.out.print("] \n");
+            System.out.print("[");
+            for (int i = 0; i < array.length; i++)
+            {
+                    array[i] = (int)(Math.random() * 501);
+                    System.out.print(" " + array[i] + " ");
+            }
+            System.out.print("] \n");
 	}
 	
 	public static void bubbleSortDoWhile(int[] array)
 	{
-		int temp;
-		boolean troca = false;
-		
-		do{
-			troca = false;
-			for(int i = 0; i < array.length - 1; i++)
-			{
-				if(array[i] > array[i + 1])
-				{
-					temp = array[i];
-					array[i] = array[i + 1];
-					array[i + 1] = temp;
-					troca = true;
-				}
-			}
-		}while(troca);
-		
-		System.out.print("[");
-		for(int i : array)
-		{
-			System.out.print(" " + i + " ");	
-		}
-		System.out.print("] \n");
+            int temp;
+            boolean troca = false;
+
+            do{
+                    troca = false;
+                    for(int i = 0; i < array.length - 1; i++)
+                    {
+                            if(array[i] > array[i + 1])
+                            {
+                                    temp = array[i];
+                                    array[i] = array[i + 1];
+                                    array[i + 1] = temp;
+                                    troca = true;
+                            }
+                    }
+            }while(troca);
+
+            System.out.print("[");
+            for(int i : array)
+            {
+                    System.out.print(" " + i + " ");	
+            }
+            System.out.print("] \n");
 	}
 	
 	public static void bubbleSortFor(int[] array)
 	{
-		int temp;
-		
-		for(int i = 0; i < array.length; i++)
-		{
-			for(int j = i + 1; j < array.length; j++)
-			{
-				if(array[i] > array[j])
-				{
-					temp = array[j];
-					array[j]= array[i];
-					array[i] = temp;
-				}
-			}
-		}
-		
-		System.out.println("Bubble Sort: ");
-		System.out.print("[");
-		for(int i : array)
-		{
-			System.out.print("{" + i + "}");	
-		}
-		System.out.print("] \n");
+            int temp;
+
+            for(int i = 0; i < array.length; i++)
+            {
+                    for(int j = i + 1; j < array.length; j++)
+                    {
+                            if(array[i] > array[j])
+                            {
+                                    temp = array[j];
+                                    array[j]= array[i];
+                                    array[i] = temp;
+                            }
+                    }
+            }
+
+            System.out.println("Bubble Sort: ");
+            System.out.print("[");
+            for(int i : array)
+            {
+                    System.out.print("{" + i + "}");	
+            }
+            System.out.print("] \n");
 	}
 }
